c5ead81b6dd441dd7a59e50a041272fed988477b
EdwinZM <edwinzwa@hotmail.com>
2020-12-08 18:21:34 -0500

    ssssss

---
+++ src/Containers/Checkout/Checkout.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import {Route, Redirect} from 'react-router-dom';
+import CheckoutSummary from '../../Components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactDataFolder/ContactData';
+import {connect} from 'react-redux';
+
+
+class Checkout extends Component {
+
+    checkoutCancelledHandler = () => {
+        this.props.history.goBack();
+    }
+
+    checkoutContinuedHandler = () => {
+        this.props.history.replace('/checkout/contact-data');
+    }  
+    
+
+    render() {
+        let summary = <Redirect to='/'/>;
+         
+        if(this.props.ings) {
+            const purchasedRedirect = this.props.purchased ? <Redirect exact to='/'/> : null;
+            summary= (
+                <div>
+                 <CheckoutSummary 
+                ingredients={this.props.ings}
+                checkoutCancelled={this.checkoutCancelledHandler}
+                checkoutContinued={this.checkoutContinuedHandler}/>
+
+                <Route path={this.props.match.path + '/contact-data'} 
+                component={ContactData}
+                    />
+                    {purchasedRedirect}
+                </div>
+                
+            )
+        }
+        return summary;
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        ings: state.burgerBuilder.ingredients,
+        purchased: state.order.purchased
+    }
+};
+
+export default connect(mapStateToProps)(Checkout);
